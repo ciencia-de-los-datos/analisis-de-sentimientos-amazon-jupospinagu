a337cb5aa48480610fb73080cd789723c70c70e3
Juan Pablo Ospina Gutierrez <101611634+jupospinagu@users.noreply.github.com>
2022-05-06 17:41:39 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -137,7 +137,7 @@ def pregunta_04():
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
     # parámetros. Use cv = 5, y "accuracy" como métrica de evaluación
-    gridSearchCV = pipeline(
+    gridSearchCV = GridSearchCV(
         estimator= BernoulliNB(),
         param_grid= param_grid,
         cv= 5,
